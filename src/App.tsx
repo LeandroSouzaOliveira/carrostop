5ceb8a65e50a49a485cc86a1f6fa07a5b25d9a48
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-08-13 14:37:41 -0300

    Created and styled the HeaderClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import HeaderClient from "./components/HeaderClient";
+
 export default function App() {
   return (
-    <>
-    </>
-  )
+    <HeaderClient />
+  );
 }
---
+++ src/components/HeaderClient/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function HeaderClient() {
+    return (
+        <header>
+            <nav className="car-header-container">
+                <h1>Carros Top</h1>
+            </nav>
+        </header>
+    );
+}
